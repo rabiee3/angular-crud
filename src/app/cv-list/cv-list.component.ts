fc092c74b8985ee5bdcc80ba641336971d1de208
Rabie Mohamed <rabiee3.mohamed@gmail.com>
2023-12-01 23:18:09 +0200

    formatting data completing form

--- src/app/cv-list/cv-list.component.ts
+++ src/app/cv-list/cv-list.component.ts
@@ -4,15 +4,21 @@ import { MatTableDataSource } from '@angular/material/table';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 interface ICV_ViewModel {
-  id?: number;
+  id: number|null;
   name: string;
   fullname: string;
+  cityName: string;
+  email: string;
+  mobilenumber: string;
 }
 
 const CV_viewModel: ICV_ViewModel = {
-  id: 1,
-  name: 'CV Name',
-  fullname: 'Full Name'
+  id: null,
+  name: 'Name',
+  fullname: 'Full Name',
+  cityName: 'City Name',
+  email: 'Email',
+  mobilenumber: 'string'
 };
 
 @Component({
@@ -28,12 +34,49 @@ export class CvListComponent implements OnInit {
   displayedPlaceholders: string[] = Object.values(CV_viewModel);
   displayedColumns: string[] = Object.keys(CV_viewModel);
 
-  dummyData = [
-    { id: 1, name: 'Ali', fullname: 'Ali Mohamed' },
-    { id: 2, name: 'Rabie', fullname: 'Rabie Mohamed' },
-    { id: 3, name: 'Salah', fullname: 'Salah Moussa' }
-  ];
-  dataSource = new MatTableDataSource<ICV_ViewModel>(this.dummyData);
+  responseData = {
+    items: [
+      {
+        id: 5,
+        name: 'Ali',
+        personalInformationId: 1,
+        experienceInformationId: 0,
+        personalInformation: {
+          id: 1,
+          fullName: 'Jaber',
+          cityName: 'Fayoum',
+          email: 'alimjbr1@gmail.com',
+          mobileNumber: '01062701022',
+          cvId: 1
+        },
+        experienceInformation: [
+          {
+            id: 2,
+            companyName: 'ElevateTCS',
+            city: 'Cairo',
+            companyField: 'Telecom',
+            cvId: 5
+          }
+        ]
+      }
+    ],
+    metaData: {
+      pageCount: 1,
+      totalItemCount: 1,
+      pageNumber: 1,
+      pageSize: 10,
+      hasPreviousPage: false,
+      hasNextPage: false,
+      isFirstPage: true,
+      isLastPage: true,
+      firstItemOnPage: 1,
+      lastItemOnPage: 1
+    }
+  };
+
+  dummyData: any = [];
+
+  dataSource = new MatTableDataSource<ICV_ViewModel>([]);
 
   constructor(
     private fb: FormBuilder,
@@ -41,12 +84,12 @@ export class CvListComponent implements OnInit {
     private modalService: BsModalService
   ) {
     this.cvForm = this.fb.group({
-      id: [''],
-      name: ['', [Validators.required]],
-      fullname: ['', [Validators.required]],
-      cityname: ['', [Validators.required]],
-      email: ['', [Validators.required]],
-      mobilenumber: ['', [Validators.required]]
+      id: [null],
+      name: [null, [Validators.required]],
+      fullname: [null, [Validators.required]],
+      cityname: [null, [Validators.required]],
+      email: [null, [Validators.required]],
+      mobilenumber: [null, [Validators.required]]
     });
   }
 
@@ -57,7 +100,25 @@ export class CvListComponent implements OnInit {
 
   ngOnInit(): void {
     this.displayedColumns.push('actions');
-    console.log(this.displayedColumns);
+
+    this.getData();
+  }
+
+  formatData(data: any) {
+    return data.map((item: any) => {
+      return {
+        id: item.id,
+        name: item.name,
+        fullname: item.personalInformation.fullName,
+        cityname: item.personalInformation.cityName,
+        email: item.personalInformation.email,
+        mobilenumber: item.personalInformation.mobileNumber
+      };
+    });
+  }
+
+  getData() {
+    this.dataSource = this.formatData(this.responseData.items)
   }
 
   addCV() {
